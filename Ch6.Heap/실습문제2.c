c4601c99649f2c86e74b70098a115d1e73352e9e
Gwanyeop Kim <81212131+cedric0708@users.noreply.github.com>
2022-09-19 00:55:37 +0900

    Create 실습문제2.c

---
+++ Ch6.Heap/실습문제2.c
@@ -0,0 +1,59 @@
+#pragma warning(disable:4996)
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int h[100] = { 0 };
+int n=0;
+
+int isexternal(int idx) {
+	return (2 * (idx) > n);
+}
+
+void downheap(int i) {
+	int s = 2 * i + 1;
+	if (isexternal(2 * i + 1)) {
+		if (h[2 * i] > h[s])
+			s = 2 * i;
+	}
+	if (h[i] >= h[s])
+		return;
+	int tmp = h[i];
+	h[i] = h[s];
+	h[s] = tmp;
+	downheap(s);
+}
+
+void rbuildheap(int i) {
+	if (i > n)
+		return;
+	rbuildheap(2 * i);
+	rbuildheap(2 * i + 1);
+	downheap(i);
+}
+
+void buildheap() {
+	for (int i = n / 2; i >= 1; i--)
+		downheap(i);
+}
+
+void printheap() {
+	for (int i = 1; i <= n; i++)
+		printf(" %d", h[i]);
+	printf("\n");
+}
+
+int main()
+{
+	int d;
+	scanf("%d", &d);
+	n = d;
+
+	for (int i = 1; i <= d; i++) {
+		scanf("%d", &h[i]);
+	}
+	rbuildheap(1);
+	printheap();
+
+	return 0;
+}
