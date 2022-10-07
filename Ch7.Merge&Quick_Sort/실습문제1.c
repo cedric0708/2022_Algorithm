29d9c8c82485f6783f21f53e1740d27bf970491f
Gwanyeop Kim <81212131+cedric0708@users.noreply.github.com>
2022-10-08 01:27:38 +0900

    Update 실습문제1.c

--- Ch7.Merge&Quick_Sort/실습문제1.c
+++ Ch7.Merge&Quick_Sort/실습문제1.c
@@ -3,130 +3,114 @@
 #include <string.h>
 #include <stdlib.h>
 
-int n;
-
 typedef struct listnode {
 	int elem;
 	struct listnode* next;
 }listnode;
 
-typedef struct listype {
-	listnode* h;
-}listype;
-
-void init(listype* l) {
-	l->h = NULL;
-}
-
-int isempty(listype* l) {
-	return l->h == NULL;
-}
-
-int listsize(listype* l) {
-	listnode* p = l->h;
-	int c = 0;
-	if (p == NULL)
-		return 0;
-	else {
-		while (p->next != NULL) {
-			p = p->next;
-			c++;
-		}
-		return c+1;
-	}
-}
-
-int get(listype *l, int pos) {
-	listnode* p = l->h;
-	return p->elem;
-}
-
-void insertlast(listype* l, int e) {
+void insertlast(listnode** l, int e) {
 	listnode* node = (listnode*)malloc(sizeof(listnode));
 	node->elem = e;
 	node->next = NULL;
 
-	listnode* p = l->h;
-	if (p == NULL)
-		l->h = node;
+	listnode* p = *l;
+	if (*l == NULL)
+		*l = node;
 	else {
 		while (p->next != NULL)
 			p = p->next;
 		p->next = node;
 	}
-}//맨 뒤에 삽입
+}
 
-int deletefirst(listype* l) {
-	listnode* p = l->h;
-
-	int e = p->elem;
-	l->h = p->next;
-	free(p);
-	return e;
-}//맨 앞 삭제
-
-void partition(listype* l, listype* l1, listype* l2, int n) {
-	listnode* p = l->h;
-
-	l1 = l;
+void partition(listnode* l, listnode** l1, listnode** l2, int n) {
+	listnode* p = l;
+	*l1 = l;
 
 	for (int i = 0; i < (n / 2) - 1; i++) {
-
 		p = p->next;
-
 	}
-
-	l2 = p->next;
-
+	
+	*l2 = p->next;
 	p->next = NULL;
 }
 
-
-listype* merge(listype *l1, listype *l2) {
-	listype L;
-	init(&L);
-	while (!isempty(l1) && !isempty(l2)) {
-		if (get(l1, 1) <= get(l2, 1))
-			insertlast(&L, deletefirst(l1));
+listnode* merge(listnode** l1, listnode** l2) {
+	listnode *L, *p, *a=*l1, *b=*l2;
+	if (a->elem <= b->elem) {
+		L = a;
+		a = a->next;
+		p = L;
+	}
+	else {
+		L = b;
+		b = b->next;
+		p = L;
+	}
+	while (a!=NULL && b!=NULL) {
+		if (a->elem <= b->elem) {
+			p->next = a;
+			a = a->next;
+			p = p->next;
+		}
 		else
-			insertlast(&L, deletefirst(l2));
+		{
+			p->next = b;
+			b = b->next;
+			p = p->next;
+		}
 	}
-	while(!isempty(l1))
-		insertlast(&L, deletefirst(l1));
-	while (!isempty(l2))
-		insertlast(&L, deletefirst(l2));
-	return &L;
+	while (a != NULL)
+	{
+		p->next = a;
+		a = a->next;
+		p = p->next;
+	}
+	while (b != NULL)
+	{
+		p->next = b;
+		b = b->next;
+		p = p->next;
+	}
+	return L;
 }
 
-void mergesort(listype *l) {
-	listype l1, l2;
-	init(&l1);
-	init(&l2);
-	if (listsize(l) > 1) {
-		partition(l, &l1, &l2, n);
-		mergesort(&l1);
-		mergesort(&l2);
-		l = merge(&l1, &l2);
+void mergesort(listnode** l, int n) {
+	listnode *l1, *l2, *p=*l;
+	if (n > 1) {
+		partition(p, &l1, &l2, n);
+		if (n % 2 == 0) {
+			mergesort(&l1, n / 2);
+			mergesort(&l2, n / 2);
+		}
+		else {
+			mergesort(&l1, n / 2);
+			mergesort(&l2, (n / 2) + 1);
+		}
+		*l = merge(&l1, &l2);
 	}
 }
 
-void print(listype* l) {
-	for (listnode* p = l->h; p->next != NULL; p = p->next)
+void print(listnode* l) {
+	listnode* p = l;
+	while (p != NULL) {
 		printf(" %d", p->elem);
+		p = p->next;
+	}
 }
 
 
 int main()
 {
-	listype l;
-	init(&l);
-	int d;
+	listnode *l=NULL;
+	int n, d;
 	scanf("%d", &n);
 	for (int i = 0; i < n; i++) {
 		scanf("%d", &d);
 		insertlast(&l, d);
 	}
-	mergesort(&l);
+	mergesort(&l, n);
+	print(l);
 
 	return 0;
 }
